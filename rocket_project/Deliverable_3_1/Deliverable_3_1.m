9532fde89de918c88e7e6645c6eadaa4b15577b3
mschertenleib <schertenleib.mathieu@gmail.com>
2023-12-25 18:35:02 +0100

    Fill Deliverable_3_2.m, update initial states names

--- rocket_project/Deliverable_3_1/Deliverable_3_1.m
+++ rocket_project/Deliverable_3_1/Deliverable_3_1.m
@@ -17,19 +17,19 @@ mpc_z = MpcControl_z(sys_z, Ts, H);
 mpc_roll = MpcControl_roll(sys_roll, Ts, H);
 
 Tf = 7.0;
-x0_x = [0, 0, 0, 3]';
-x0_y = [0, 0, 0, 3]';
-x0_z = [0, 3]';
-x0_roll = [0, deg2rad(40)]';
+x0 = [0, 0, 0, 3]';
+y0 = [0, 0, 0, 3]';
+z0 = [0, 3]';
+roll0 = [0, deg2rad(40)]';
 
 rocket.anim_rate = 2.0;
-[T, X_sub, U_sub] = rocket.simulate_f(sys_x, x0_x, Tf, @mpc_x.get_u, 0);
+[T, X_sub, U_sub] = rocket.simulate_f(sys_x, x0, Tf, @mpc_x.get_u, 0);
 rocket.plotvis_sub(T, X_sub, U_sub, sys_x, xs, us);
-[T, X_sub, U_sub] = rocket.simulate_f(sys_y, x0_y, Tf, @mpc_y.get_u, 0);
+[T, X_sub, U_sub] = rocket.simulate_f(sys_y, y0, Tf, @mpc_y.get_u, 0);
 rocket.plotvis_sub(T, X_sub, U_sub, sys_y, xs, us);
-[T, X_sub, U_sub] = rocket.simulate_f(sys_z, x0_z, Tf, @mpc_z.get_u, 0);
+[T, X_sub, U_sub] = rocket.simulate_f(sys_z, z0, Tf, @mpc_z.get_u, 0);
 rocket.plotvis_sub(T, X_sub, U_sub, sys_z, xs, us);
-[T, X_sub, U_sub] = rocket.simulate_f(sys_roll, x0_roll, Tf, @mpc_roll.get_u, 0);
+[T, X_sub, U_sub] = rocket.simulate_f(sys_roll, roll0, Tf, @mpc_roll.get_u, 0);
 rocket.plotvis_sub(T, X_sub, U_sub, sys_roll, xs, us);
 
 %% TODO: This file should produce all the plots for the deliverable
--- rocket_project/Deliverable_3_2/Deliverable_3_2.m
+++ rocket_project/Deliverable_3_2/Deliverable_3_2.m
@@ -17,19 +17,23 @@ mpc_z = MpcControl_z(sys_z, Ts, H);
 mpc_roll = MpcControl_roll(sys_roll, Ts, H);
 
 Tf = 7.0;
-x0_x = [0, 0, 0, 3]';
-x0_y = [0, 0, 0, 3]';
-x0_z = [0, 3]';
-x0_roll = [0, deg2rad(40)]';
+x0 = zeros(4, 1);
+ref_x = [0, 0, 0, -4]';
+y0 = zeros(4, 1);
+ref_y = [0, 0, 0, -4]';
+z0 = zeros(2, 1);
+ref_z = [0, -4]';
+roll0 = zeros(2, 1);
+ref_roll = [0, deg2rad(35)]';
 
 rocket.anim_rate = 2.0;
-[T, X_sub, U_sub] = rocket.simulate_f(sys_x, x0_x, Tf, @mpc_x.get_u, 0);
-rocket.plotvis_sub(T, X_sub, U_sub, sys_x, xs, us);
-[T, X_sub, U_sub] = rocket.simulate_f(sys_y, x0_y, Tf, @mpc_y.get_u, 0);
-rocket.plotvis_sub(T, X_sub, U_sub, sys_y, xs, us);
-[T, X_sub, U_sub] = rocket.simulate_f(sys_z, x0_z, Tf, @mpc_z.get_u, 0);
-rocket.plotvis_sub(T, X_sub, U_sub, sys_z, xs, us);
-[T, X_sub, U_sub] = rocket.simulate_f(sys_roll, x0_roll, Tf, @mpc_roll.get_u, 0);
-rocket.plotvis_sub(T, X_sub, U_sub, sys_roll, xs, us);
+[T, X_sub, U_sub] = rocket.simulate_f(sys_x, x0, Tf, @mpc_x.get_u, ref_x);
+rocket.plotvis_sub(T, X_sub, U_sub, sys_x, xs, us, ref_x);
+[T, X_sub, U_sub] = rocket.simulate_f(sys_y, y0, Tf, @mpc_y.get_u, ref_y);
+rocket.plotvis_sub(T, X_sub, U_sub, sys_y, xs, us, ref_y);
+[T, X_sub, U_sub] = rocket.simulate_f(sys_z, z0, Tf, @mpc_z.get_u, ref_z);
+rocket.plotvis_sub(T, X_sub, U_sub, sys_z, xs, us, ref_z);
+[T, X_sub, U_sub] = rocket.simulate_f(sys_roll, roll0, Tf, @mpc_roll.get_u, ref_roll);
+rocket.plotvis_sub(T, X_sub, U_sub, sys_roll, xs, us, ref_roll);
 
 %% TODO: This file should produce all the plots for the deliverable
