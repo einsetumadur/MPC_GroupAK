ea6e634036c0d07e3ce365c11faaef24f53d80de
Gilles Regamey <gillesoff@gmail.com>
2023-12-30 12:02:44 +0100

    Working 5.1
    note : in mpc_Z , the observer is not nominal

--- rocket_project/Deliverable_5_1/Deliverable_5_1.m
+++ rocket_project/Deliverable_5_1/Deliverable_5_1.m
@@ -1,7 +1,39 @@
 addpath(fullfile('..', 'src'));
 
-%close all
-%clear all
-%clc
+close all;
+clear;
+clc;
+
+Ts = 1/20;
+rocket = Rocket(Ts);
+[xs, us] = rocket.trim();
+sys = rocket.linearize(xs, us);
+[sys_x, sys_y, sys_z, sys_roll] = rocket.decompose(sys, xs, us);
+
+H = 5.0;
+mpc_x = MpcControl_x(sys_x, Ts, H);
+mpc_y = MpcControl_y(sys_y, Ts, H);
+mpc_roll = MpcControl_roll(sys_roll, Ts, H);
+
+%%
+mpc_z = MpcControl_z(sys_z, Ts, H);
+
+% Merge four sub−system controllers into one full−system controller
+mpc = rocket.merge_lin_controllers(xs, us, mpc_x, mpc_y, mpc_z, mpc_roll);
+
+x0 = zeros(12, 1);
+
+% Setup reference function
+ref = @(t_, x_) ref_TVC(t_);
+
+% Simulate
+Tf = 30;
+rocket.mass = 2.13;
+[T, X, U, Ref] = rocket.simulate_est_z(x0, Tf, @mpc.get_u, ref,mpc_z,sys_z);
+
+% Visualize
+rocket.anim_rate = 5; % Increase this to make the animation faster
+ph = rocket.plotvis(T, X, U, Ref);
+ph.fig.Name = 'Merged lin. MPC in nonlinear simulation with const disturbance'; % Set a figure title
 
 %% TODO: This file should produce all the plots for the deliverable
