247334c3411ab459234b5781c2bbd03872420312
mschertenleib <schertenleib.mathieu@gmail.com>
2024-01-08 18:22:43 +0100

    Prototype implementation of delay-compensating controller

--- rocket_project/Deliverable_6_2/Deliverable_6_2.m
+++ rocket_project/Deliverable_6_2/Deliverable_6_2.m
@@ -8,17 +8,18 @@ Ts = 1/40;
 rocket = Rocket(Ts);
 
 H = 0.5;
-nmpc = NmpcControl(rocket, H);
+expected_delay = 3;
+nmpc = NmpcControl(rocket, H, expected_delay);
 
 x0 = zeros(12, 1);
 ref = [0.5, 0, 1, deg2rad(65)]';
-
 Tf = 2.5;
+
 rocket.mass = 1.75;
-rocket.delay = 1;
+rocket.delay = 3;
 [T, X, U, Ref] = rocket.simulate(x0, Tf, @nmpc.get_u, ref);
 
-% rocket.anim_rate = 1;
+rocket.anim_rate = 1;
 rocket.plotvis(T, X, U, Ref);
 
 %% TODO: This file should produce all the plots for the deliverable
